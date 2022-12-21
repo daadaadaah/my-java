aa7c04b9950053b6ca4f0b9ecaeb017de21a1d88
daadaadaah <daadaadaah@gmail.com>
2022-12-21 22:55:39 +0900

    [Ch2.Hello God of Java] 다히 풀이 추가

---
+++ god-of-java/Chapter02/practice/HelloGodOfJava.java
@@ -0,0 +1,5 @@
+public class HelloGodOfJava {
+  public static void main(String[] args) {
+		System.out.println("Hello God of Java !!");
+	}
+}
---
+++ god-of-java/Chapter02/practice/Profile.java
@@ -0,0 +1,6 @@
+public class Profile {
+  public static void main(String[] args) {
+		System.out.println("My name is daadaadaah");
+    System.out.println("My age is 1");
+	}
+}
