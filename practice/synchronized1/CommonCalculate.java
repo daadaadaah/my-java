63ade61972181c397b42826c0f27b7f7b506525e
daadaadaah <daadaadaah@gmail.com>
2023-02-19 23:11:43 +0900

    문제 : 동일한 20000이 나올꺼라고 예상했지만, 12935, 15370 등 예측할 수 없는 숫자들이 나왔다. - 또한 실행할 때마다 다른 숫자가 나옴
    > 출력 결과
    calc : 12935
    calc : 15370
    calc : 14816
    calc : 14773
    calc : 12905

---
+++ practice/synchronized1/CommonCalculate.java
@@ -0,0 +1,21 @@
+package synchronized1;
+
+public class CommonCalculate {
+  private int amount;
+  
+  public CommonCalculate() {
+    this.amount = 0;
+  }
+
+  public void plus(int value) {
+    this.amount += value;
+  }
+  
+  public void minus(int value) {
+    this.amount -= value;
+  }
+
+  public int getAmount() {
+    return this.amount;
+  }
+}
