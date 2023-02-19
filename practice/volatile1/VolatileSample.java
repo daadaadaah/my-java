09c4cb2d48419669c763e0229e0914faeaa9f4a9
daadaadaah <daadaadaah@gmail.com>
2023-02-19 17:58:32 +0900

    [volatile 추가] 아래처럼 출력되고 sample 쓰레드는 종료된다.
    > 출력 결과
    --------------------- VolatileSample : run() start
    RunVolatitle : sleep() end
    RunVolatitle : Set value is completed !
    --------------------- VolatileSample : instanceVariable : -1.0
    --------------------- VolatileSample : run() end

--- practice/volatile1/VolatileSample.java
+++ practice/volatile1/VolatileSample.java
@@ -2,7 +2,7 @@ package volatile1;
 
 public class VolatileSample extends Thread {
 
-  private double instanceVariable = 0;
+  private volatile double instanceVariable = 0;
   
   void setDouble(double value) {
     this.instanceVariable = value;
@@ -11,13 +11,7 @@ public class VolatileSample extends Thread {
   public void run() {
     System.out.println("--------------------- VolatileSample : run() start ");
     
-    try {
-      while(instanceVariable == 0) {
-        Thread.sleep(1);      
-      }
-    } catch(Exception e) {
-      e.printStackTrace();
-    }
+    while(instanceVariable == 0);
 
     System.out.println("--------------------- VolatileSample : instanceVariable : "+instanceVariable);
 
