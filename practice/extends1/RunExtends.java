23851c02a54d6a3c0cc91b71feae735755fe3802
daadaadaah <daadaadaah@gmail.com>
2023-02-21 00:06:58 +0900

    protected로 선언된 메서드는 상속 구조가 아닌 클래스에서는 접근 불가능하다.

--- practice/extends1/RunExtends.java
+++ practice/extends1/RunExtends.java
@@ -15,5 +15,9 @@ public class RunExtends {
     // 3. 손자의 경우
     GrandChild grandChild = new GrandChild();
     grandChild.printProtectedName();
+
+    // 4. 상속 구조가 아닌 경우
+    Friend friend = new Friend();
+//    friend.printProtectedName(); 불가능!
   }
 }
