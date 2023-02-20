ff04ff218cffdeed30b140c9f4e1e5a5f0c3eaab
daadaadaah <daadaadaah@gmail.com>
2023-02-21 00:25:59 +0900

    부모 클래스 보다 접근 제어자가 축소되는 것은 문제가 된다. protected -> pacakge-private로 축소 됨

--- practice/extends1/Child.java
+++ practice/extends1/Child.java
@@ -4,6 +4,9 @@ public class Child extends Parent {
     public Child() {
         System.out.println("Child Constructor");
     }
-
-
+    
+    @Override
+    void printProtectedName() { //  attempting to assign weaker access privileges ('package-private'); was 'protected' 라고 하면서 에러 남!
+        System.out.println("Parent printProtectedName()");
+    }
 }
