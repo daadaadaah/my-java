ef8320964d4e0a96201525159e5849d42c8c96f2
daadaadaah <daadaadaah@gmail.com>
2023-02-21 00:31:13 +0900

    부모는 자식을 담을 수 있으나, 자식은 부모를 담지 못한다.
    왜냐하면, 자식에게만 존재하는 변수나 메서드가 있는데, 이를 부모로부터 참조할 수 없으므로,

--- practice/extends1/RunExtends.java
+++ practice/extends1/RunExtends.java
@@ -3,21 +3,28 @@ package extends1;
 public class RunExtends {
   public static void main(String[] args) {
     // 1. 매개변수가 없는 경우
-//    Child child = new Child();
+    Child child = new Child();
 //    child.printName();
 ////    child.printPrivateName(); 불가능!
-//    child.printProtectedName();
+    child.printProtectedName();
 
     // 2. 매개변수가 있는 경우
     ChildArg childArg = new ChildArg();
     childArg.printName();
 
-    // 3. 손자의 경우
-    GrandChild grandChild = new GrandChild();
-    grandChild.printProtectedName();
+//    // 3. 손자의 경우
+//    GrandChild grandChild = new GrandChild();
+//    grandChild.printProtectedName();
 
     // 4. 상속 구조가 아닌 경우
     Friend friend = new Friend();
 //    friend.printProtectedName(); 불가능!
+
+    // 5. 형변환
+    // (1) 가능
+    Parent parent = new Child();
+
+    // (2) 불가능
+    Child child = new Parent();
   }
 }
