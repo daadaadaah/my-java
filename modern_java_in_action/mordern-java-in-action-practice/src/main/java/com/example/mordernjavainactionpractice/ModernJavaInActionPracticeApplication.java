59edd0a668d6f40a09c89bc69f44b879c92b7fb8
daadaadaah <daadaadaah@gmail.com>
2023-10-18 19:11:55 +0900

    연습 프로젝트 초기화

---
+++ modern_java_in_action/mordern-java-in-action-practice/src/main/java/com/example/mordernjavainactionpractice/ModernJavaInActionPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.example.mordernjavainactionpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernJavaInActionPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernJavaInActionPracticeApplication.class, args);
+	}
+}
