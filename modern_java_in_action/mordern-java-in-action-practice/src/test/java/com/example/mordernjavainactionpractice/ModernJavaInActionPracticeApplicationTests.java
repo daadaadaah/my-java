f8a64f8f0f744126c14486c5d8a31eddfdfc26da
daadaadaah <daadaadaah@gmail.com>
2023-10-18 19:11:55 +0900

    연습 프로젝트 초기화

---
+++ modern_java_in_action/mordern-java-in-action-practice/src/test/java/com/example/mordernjavainactionpractice/ModernJavaInActionPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mordernjavainactionpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernJavaInActionPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
