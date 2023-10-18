7f46bbbb98342cb011e92859b2a622f0fa304e37
daadaadaah <daadaadaah@gmail.com>
2023-10-18 19:23:03 +0900

    [모던 자바 인 액션] 1번째 시도 : 녹색 사과 필터

--- modern_java_in_action/mordern-java-in-action-practice/src/main/java/com/example/mordernjavainactionpractice/ModernJavaInActionPracticeApplication.java
+++ modern_java_in_action/mordern-java-in-action-practice/src/main/java/com/example/mordernjavainactionpractice/ModernJavaInActionPracticeApplication.java
@@ -1,12 +1,32 @@
 package com.example.mordernjavainactionpractice;
 
-import org.springframework.boot.SpringApplication;
+import static com.example.mordernjavainactionpractice.Color.GREEN;
+import static com.example.mordernjavainactionpractice.Color.RED;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ModernJavaInActionPracticeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ModernJavaInActionPracticeApplication.class, args);
+		List<Apple> inventory = Arrays.asList(new Apple(80, GREEN), new Apple(155, GREEN), new Apple(120, RED));
+
+		List<Apple> greenApples = filterGreenApples(inventory);
+		System.out.println(greenApples); // [Apple{color='GREEN', weight=80}, Apple{color='GREEN', weight=155}]
+	}
+
+	public static List<Apple> filterGreenApples(List<Apple> inventory){
+		List<Apple> result = new ArrayList<>();
+
+		for(Apple apple: inventory){
+			if(GREEN.equals(apple.getColor())){
+				result.add(apple);
+			}
+		}
+		return result;
 	}
 }
