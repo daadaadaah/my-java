a65bac2406bff3d60f48e4bcc4d0e1910fe4c53e
daadaadaah <daadaadaah@gmail.com>
2024-01-31 17:21:49 +0900

    [모던 자바 인 액션] 기존 Java 7 코드

--- modern_java_in_action/mordern-java-in-action-practice/src/main/java/com/example/mordernjavainactionpractice/ModernJavaInActionPracticeApplication.java
+++ modern_java_in_action/mordern-java-in-action-practice/src/main/java/com/example/mordernjavainactionpractice/ModernJavaInActionPracticeApplication.java
@@ -1,12 +1,42 @@
 package com.example.mordernjavainactionpractice;
 
-import org.springframework.boot.SpringApplication;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ModernJavaInActionPracticeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ModernJavaInActionPracticeApplication.class, args);
+		getLowCaloricDishesNamesInJava7(Dish.menu).forEach(System.out::println);
+	}
+
+	// Java 7
+	public static List<String> getLowCaloricDishesNamesInJava7(List<Dish> dishes){
+		List<Dish> lowCaloricDishes = new ArrayList<>();
+
+		// 누적자로 요소 피러링 : 400 이하의 요리 담기
+		for(Dish d: dishes){
+			if(d.getCalories() < 400){
+				lowCaloricDishes.add(d);
+			}
+		}
+
+		// 익명 클래스로 요리 정렬 : 칼로리 순으로 정렬
+		Collections.sort(lowCaloricDishes, new Comparator<Dish>() {
+			public int compare(Dish d1, Dish d2){
+				return Integer.compare(d1.getCalories(), d2.getCalories());
+			}
+		});
+
+		// 정려된 리스트를 처리하면서 요리 이름 선택 : 요리 이름만 담기
+		List<String> lowCaloricDishesName = new ArrayList<>();
+		for(Dish d: lowCaloricDishes){
+			lowCaloricDishesName.add(d.getName());
+		}
+
+		return lowCaloricDishesName;
 	}
 }
